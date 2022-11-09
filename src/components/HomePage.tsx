63e0ffb238e8e5a60e6773a09af41b7f36d13b32
tildeeine <t1ld3me@gmail.com>
2022-11-09 18:57:14 +0100

    Første endringer

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,19 +5,19 @@ export const HomePage = () => {
   return (
     <div id="home" className="w-full h-screen bg-[#edede9]">
       <div className="max-w-[1000 px] mx-auto px-20 flex flex-col justify-center h-full">
-        <p className="text-black">Hi, my name is</p>
+        <p className="text-black">Hei, jeg heter</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-gray-700">
-          Ada Lovelace
+          Tilde Eriksen Eine
         </h1>
         <h2 className="text-3xl sm:text-6xl font-bold text-[#848794]">
-          I´m a developer.
+          Jeg er student.
         </h2>
         <p className="text-[#848794] py-4 max-w-[700px]">
-          Thanks for visiting my page, feel free to check out my portifolio
+          Takk for at du besøker siden min! Se gjerne gjennom portefølgen min
         </p>
         <div>
           <button className="text-gray-700 group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#989cab] hover:border-[#989cab] hover:text-white">
-            View Work
+            Se arbeid
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
