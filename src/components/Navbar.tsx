6f583fda4a63340d9845c27453e8141f0e47db7f
tildeeine <t1ld3me@gmail.com>
2022-11-09 20:35:21 +0100

    fikset ubrukt logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import logo from "../assets/f_logo.png";
+// import logo from "../assets/f_logo.png";
 import { Link } from "react-scroll";
 
 export const Navbar = () => {
